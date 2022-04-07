1558b1119a4e72f2aae20a7115cd89e32b86bb03
sahlitg <sahlit2006@gmail.com>
2022-04-07 15:42:12 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * printf task
+ * Return: return 0 after program execution 
+ */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
-
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
