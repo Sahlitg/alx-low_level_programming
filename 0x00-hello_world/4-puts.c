54362f809c26b2bcdbae8c7a4b6d090bc83b4b12
sahlitg <sahlit2006@gmail.com>
2022-04-07 15:09:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
+ * main - entry point
  *
- *  * main - Entry point
- *
- *  *
- *
- *  *Return: Always 0 (Success)
- *
- *  */
-
+ * Return: Always 0 (Success)
+ */
 Int main(void)
-
 {
 	Puts(“\”programming is like building a multilingual puzzle”);
-
 	Return (0);
 }
