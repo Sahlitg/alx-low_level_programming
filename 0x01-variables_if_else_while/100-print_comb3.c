f8e1bf2c49522addba6b9ab9c12d141eeeb6c37a
sahlitg <sahlit2006@gmail.com>
2022-04-08 14:32:35 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main
+ * main - Entry point
  * Return: Always (success)
  */
 int main(void)
@@ -13,7 +13,6 @@ for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-			
 if (digit1 == 8 && digit2 == 9)
 continue;
 
