f9a662eb34393690271d75aef1e7db8ccfc41aba
sahlitg <sahlit2006@gmail.com>
2022-04-13 03:50:18 -0700

    task 5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -17,6 +17,11 @@ int main(void)
 	 _putchar(' ');
 	 _putchar(r + '0');
 	 _putchar('\n');
+	 r = print_sign(0xff);
+	 _putchar(',');
+	 _putchar(' ');
+	 _putchar(r + '0');
+	 _putchar('\n');
 	 r = print_sign(-1);
 	 _putchar(',');
 	 _putchar(' ');
