3594435f93ccdc04db97c156393e3601f93b87d8
sahlitg <sahlit2006@gmail.com>
2022-04-18 10:35:20 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void _puts(char *str);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I don not fear computer. I fear the lack of them - Issac Asimov";
+	_puts(str);
+	return (0);
+}
