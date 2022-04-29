21fee6a82eaf96d5f7cbcdf1fbbd3a215d0eb197
sahlitg <sahlit2006@gmail.com>
2022-04-29 15:58:58 -0700

    fix on task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,8 +4,8 @@
 /**
  * main - Entry point
  *
- * argc: Counts the number of parameters that go into main
- * argv: Pointer of array of pointers containing strings entering main
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 
  */
 
