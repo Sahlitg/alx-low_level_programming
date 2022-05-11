36373822dc2d7a71c389e9ed84453d93c7987d98
sahlitg <sahlit2006@gmail.com>
2022-05-11 14:37:37 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: name of theperson
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	char name = "sahlit";
+
+	printf("my name is %s\n", name);
+	return ()
+}
