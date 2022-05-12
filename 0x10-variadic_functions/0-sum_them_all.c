61b3e0b33b44e5ed231363048b9541f3065c0b6c
sahlitg <sahlit2006@gmail.com>
2022-05-12 13:26:14 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,11 +5,11 @@
  *
  * return: sum or if n == 0, return 0
  */
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-	var_list ptr;
-	var_start(ptr, n);
+	va_list ptr;
+	va_start(ptr, n);
 
 	for (int i = 0; i < n; i++)
 		sum += var_arg(ptr, int);
