f323ce7d6f618c077bf227d9380a3a130103fbae
sahlitg <sahlit2006@gmail.com>
2022-05-12 13:49:58 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ptr, n);
 
 	for (int i = 0; i < n; i++)
-		sum += var_arg(ptr, int);
+		sum += va_arg(ptr, int);
 	va_end(ptr);
 	return sum;
 }
