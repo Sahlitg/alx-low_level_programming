b7c2227d328c22e562c41c55ec5e22ea36fd726d
sahlitg <sahlit2006@gmail.com>
2022-05-13 10:33:53 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int x;
-	if (array && size && action)
-	{
-		for (x = 0; x < size; x++)
-		{
-			(*action)(array[x]);
-		}
-	}
+unsigned int x;
+if (array && size && action)
+{
+for (x = 0; x < size; x++)
+{
+(*action)(array[x]);
+}
+}
 }
