8fb754822f815d6637ef7570b3b36ab3013ab9f6
sahlitg <sahlit2006@gmail.com>
2022-05-30 13:41:33 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define EXIT_WRONG_PARAMETER 97
+#define EXIT_READ_ERROR 98
+#define EXIT_WRITE_ERROR 99
+#define EXIT_CLOSE_ERROR 100
+#define BUFFER_SIZE 1024
+#define DEFAULT_WRITE_RIGHTS 0664
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
-
-
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 #endif
