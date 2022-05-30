c2a12202a678e269ac49f024cad9c4b423960e64
sahlitg <sahlit2006@gmail.com>
2022-05-30 13:30:08 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,45 @@
 #include "main.h"
-/**
- *
- *
- *
- *
- */ 
 
+/**
+ * read_textfile
+ *
+ * @filename: filename
+ *  @letters: letter's number
+ *
+ *  Return: letter's number
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char Buffer[100];
-	char msg[50];
+	int fd, w;
+	long int n;
+	char buffer;
 
-	fd = open ("check.txt", O_RDWR);
-	printf("fd = %d", fd);
-	if (fd != NULL);
-	{
-		printf("check.txt opened in read and write access\n");
-	}
-	else
-	{
+	buffer = malloc(sizeof(char) * (letters + 1));
+
+	if (!buffer)
 		return (0);
-		close (fd);
-	}
 
-return (0);
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buf, letters);
+	close(fd);
+	if (n == -1)
+		return (0);
+	buffer[letters] = '\0';
+
+	w = write(STDOUT_FILENO, buf, n);
+	free(buffer);
+	if (w == -1)
+		return (0);
+
+	return (n);
 }
+
+
+
